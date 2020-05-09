7f116ba9869f5bb9e9ea5f0770e98b5968c73309
Heather Spoelstra <hmspoelstra@gmail.com>
2020-05-09 15:40:20 -0400

    user and location data is displayed on page

--- src/components/Users.js
+++ src/components/Users.js
@@ -6,6 +6,33 @@ class Users extends Component {
     return (
       <>
         <h1>Where do you want to go?</h1>
+
+        <div className="userinfo">
+    {this.props.users.map(user => (
+    <div key={user.id}>
+        <div>Name: {user.name}</div>
+        <div>Interests: {user.interests}</div>
+
+        <table>
+        <tbody>
+        <tr>
+          <th>Wants to visit: </th> 
+          <th>Wants to see:</th> 
+          <th>Preferred time of year:</th> 
+         </tr> 
+         {user.locations.map(location => (
+         <tr key={location.id}>
+          <td>{location.city}, {location.country}</td>
+          <td>{location.landmarks}</td>
+          <td>{location.season}</td>
+          </tr>
+      ))}
+    </tbody>
+    </table>
+</div>
+        
+    ))}
+        </div>
         
       </>
     )
