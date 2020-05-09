e49f0f1dce828a004988753004ace5a4c2da8a36
Heather Spoelstra <hmspoelstra@gmail.com>
2020-05-09 12:36:58 -0400

    pulling data from api

---
+++ src/components/Users.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+  render () {
+    return (
+      <>
+        <h1>Where do you want to go?</h1>
+        
+      </>
+    )
+  }
+}
+
+export default Users
